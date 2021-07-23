6c59aadf2469949e346be753aaa8dbe865af17c7
YuvikaSaxen <84318996+YuvikaSaxen@users.noreply.github.com>
2021-07-23 20:48:09 +0530

    Update food.js

--- food.js
+++ food.js
@@ -5,9 +5,10 @@ display(){
    var button= createButton("Feed The Dog");
    button.position(400,125);
    if(button.mousePressed(Function(){
-                         foodS=foodS-1;
-                          
-                         ))
+   foodS=foodS-1;
+   gameState=1;
+   database.ref('/').update({'gameState':gameState})
+      }));
    var addFood= createButton("Add Food");
 addFood.position(500,125);
 if(addFood.mousePressed(function(){
